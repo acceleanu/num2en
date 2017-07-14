091dc9f806b2652f69f595891fec68a7c4715b53
Dan Acceleanu <acceleanu@gmail.com>
2017-07-14 04:44:08 +0100

    Add simple test cases and make the tests pass

--- src/num2en.clj
+++ src/num2en.clj
@@ -1,5 +1,14 @@
 (ns num2en)
 
 (defn num2en
-  ([]  "Usage: num2en <number>")
+  ([] "Usage: num2en <number>")
+  ([n]
+   (cond
+     (= n 0) "zero"
+     (= n 1) "one"
+     :else "TODO"
+     )
+    )
   )
+
+
--- test/num2en_test.clj
+++ test/num2en_test.clj
@@ -6,3 +6,16 @@
   (is (= (num2en) "Usage: num2en <number>"))
 )
 
+(deftest convert-numbers-0-19-test
+  (is (= (num2en 0) "zero"))
+  (is (= (num2en 1) "one"))
+  ;(is (= (num2en 2) "two"))
+  ;(is (= (num2en 3) "three"))
+  ;(is (= (num2en 4) "four"))
+  ;(is (= (num2en 5) "five"))
+  ;(is (= (num2en 6) "six"))
+  ;(is (= (num2en 7) "seven"))
+  ;(is (= (num2en 8) "eight"))
+  ;(is (= (num2en 9) "nine"))
+  ;(is (= (num2en 10) "ten"))
+  )
